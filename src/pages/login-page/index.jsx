33dd98d217d76209bdd884f775a42a913b6e68ab
adityasingh92 <iamaditya332@yahoo.com>
2022-07-16 22:53:48 +0530

    inital commit

---
+++ src/pages/login-page/index.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const LoginPage = () => {
+    return (
+        <Fragment>
+            <div>
+                Login Page
+            </div>
+        </Fragment>
+    );
+};
+
+export default LoginPage; 
